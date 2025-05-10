2ff00bda9c96365fa8afb1cd93498eef0073903a
Joan <joan2003024@gmail.com>
2025-05-10 17:56:52 +0200

    add allowed_host

--- tasks_backend/settings.py
+++ tasks_backend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-03ayc8k2095sf_qpp9(im5%*%yud(w^vzcug1v2sc@v%y+9$a1
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["127.0.0.1"]
 
 
 # Application definition
